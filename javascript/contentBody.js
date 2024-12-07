da779aa218bb8fa4455fdc8cb304491c88ebc0ad
matteo <matteo.massimello@gmail.com>
2024-12-07 23:18:42 +0100

    fetch: try a second page

--- javascript/contentBody.js
+++ javascript/contentBody.js
@@ -5,7 +5,7 @@ let navbar = `<div id="nav-bar" class="nav-bar">
 
         <div id="repository" class="repository">
             <div id="home" class="home">cd home</div>
-            <div id="blog" class="blog">cd ~/thoughts</div>
+            <div onover="" id="blog" class="blog">cd ~/thoughts</div>
             <div id="project" class="project">cd ~/project</div>
             <div id="about" class="about">cd ~/about</div>
         </div>
