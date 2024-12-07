da7f168989081e3be8fe88048ea9b66a85bfad08
matteo <matteo.massimello@gmail.com>
2024-12-07 23:39:00 +0100

    fetch: resolve some bugs

--- javascript/contentBody.js
+++ javascript/contentBody.js
@@ -4,10 +4,10 @@ let navbar = `<div id="nav-bar" class="nav-bar">
         </div>
 
         <div id="repository" class="repository">
-            <div id="home" class="home"><a href="#">cd home</a></div>
-            <div id="blog" class="blog"><a href="/thoughts.html">cd ~/thoughts</a></div>
-            <div id="project" class="project"><a href="#">cd ~/project</a></div>
-            <div id="about" class="about"><a href="#">cd ~/about</a></div>
+            <div id="home" class="home"><a id="ahome" href="./index.html">cd home</a></div>
+            <div id="blog" class="blog"><a id="ablog" href="./thoughts.html">cd ~/thoughts</a></div>
+            <div id="project" class="project"><a id="apoject" href="#">cd ~/project</a></div>
+            <div id="about" class="about"><a id="aabout" href="#">cd ~/about</a></div>
         </div>
     </div>
 
