16976fbe0e17f430f6fefe8a21fb850abc084b51
matteo <matteo.massimello@gmail.com>
2024-12-07 22:53:47 +0100

    fetch: done some good thinghs, nav-bar and a template base like js

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 function init() {
-
-
-
+    document.getElementById("navbar").innerHTML = navbar;
+    document.getElementById("body").innerHTML = mainPage;
+    let root = document.getElementById("home");
+    root.style.background = "#8839ef";
 }
